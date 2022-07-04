aa4cfa3c1589189fce808e5b80460317015292e4
MrDustinMiller <dustin.miller@mscoding.org>
2022-07-04 17:05:43 -0500

    for mobile nav

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,3 +1,8 @@
+let hamburgerNav = document.querySelector(".fa-bars");
+let links = document.querySelector(".nav-list");
+
 function downloadCV() {
     alert('downloading cover letter');
 }
+
+hamburgerNav.addEventListener('click', myFunction());
