d9c41f748368bf8899032ec5782bd567641663b2
MrDustinMiller <dustin.miller@mscoding.org>
2022-07-06 14:57:39 -0500

    clean code

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,8 +1,4 @@
-let hamburgerNav = document.querySelector(".fa-bars");
-let links = document.querySelector(".nav-list");
-
 function downloadCV() {
     alert('downloading cover letter');
 }
 
-hamburgerNav.addEventListener('click', myFunction());
---
+++ scripts/nav.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".fa-bars");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener('click', () => {
+   alert('under construction')
+})
