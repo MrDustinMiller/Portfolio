fa5ecb4b19a3caff93b56d22fb1563f660e3360f
MrDustinMiller <dustin.miller@mscoding.org>
2022-08-17 16:33:25 -0500

    added visibility styling to mobile nav

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,4 +1,8 @@
+let nav = document.getElementById("myLinks");
 function downloadCV() {
-    alert('downloading cover letter');
+  alert("downloading cover letter");
 }
 
+nav.addEventListener("click", (e) => {
+  x.style.visibility = "hidden";
+});
